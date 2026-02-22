da6572da4576fb7f0452aaccd0d9f9fadf852334
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-02-22 09:27:55 +0530

    refactor: change id to orderId and add custId in Order class

--- src/main/java/edu/icet/model/Order.java
+++ src/main/java/edu/icet/model/Order.java
@@ -3,14 +3,13 @@ package edu.icet.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @Data
 @AllArgsConstructor
 public class Order {
-
-    private int id;
+    private String orderId;
     private LocalDate date;
+    private String custId;
 }
